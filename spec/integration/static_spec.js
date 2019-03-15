69b55734f2a5766275d59f99b4326bc9e32eed48
lawrence <lawrence.t.yen@gmail.com>
2019-03-15 16:41:57 -0700

    intro-express-assignment-complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,8 @@
 const request = require('request');
 const server = require('../../src/server');
 const base = 'http://localhost:3000/';
+const marco = 'http://localhost:3000/marco';
+
 
 describe('routes : static', () => {
 
@@ -12,5 +14,23 @@ describe('routes : static', () => {
         done();
       })
     })
+  })
+
+  describe('GET /marco', () => {
+
+    it('Should return status code 200', (done) => {
+      request.get(marco, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done()
+      });
+    });
+
+    it('Should return polo in the body', (done) => {
+      request.get(marco, (err, res, body) => {
+        expect(body).toBe('polo');
+        done();
+      })
+    })
+
   })
 })
