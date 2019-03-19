82b2243925d86cc986082be275877c1e67c1795a
lawrence <lawrence.t.yen@gmail.com>
2019-03-19 13:28:02 -0700

    topic-pt1-help

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   init(app) {
     const staticRoutes = require('../routes/static.js');
+    const topicRoutes = require('../routes/topics.js');
+
     app.use(staticRoutes);
+    app.use(topicRoutes);
   }
 }
