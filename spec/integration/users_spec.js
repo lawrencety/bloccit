2dc1b31545b5da56f47f619f618ae50ca95d8fb3
lawrence <lawrence.t.yen@gmail.com>
2019-04-03 13:22:55 -0700

    profiles

--- spec/integration/users_spec.js
+++ spec/integration/users_spec.js
@@ -1,8 +1,11 @@
 const request = require('request');
 const server = require('../../src/server');
 const base = 'http://localhost:3000/users/';
-const User = require('../../src/db/models').User;
 const sequelize = require('../../src/db/models/index').sequelize;
+const User = require('../../src/db/models').User;
+const Topic = require('../../src/db/models').Topic;
+const Post = require('../../src/db/models').Post;
+const Comment = require('../../src/db/models').Comment;
 
 describe('routes: users', () => {
 
@@ -82,5 +85,52 @@ describe('routes: users', () => {
     })
   })
 
+  describe('GET /users/:id', () => {
+
+    beforeEach((done) => {
+      this.user;
+      this.post;
+      this.comment;
+      User.create({
+        email: 'starman@tesla.com',
+        password: 'Trekkie4lyfe'
+      })
+      .then((user) => {
+        this.user = user;
+        Topic.create({
+          title: 'Winter Games',
+          description: 'Post your Winter Games stories.',
+          posts: [{
+            title: 'Snowball Fighting',
+            body: 'So much snow',
+            userId: this.user.id
+          }]
+        }, {
+          include: { model: Post, as: 'posts' }
+        })
+        .then((topic) => {
+          this.post = topic.posts[0];
+          Comment.create({
+            body: 'This comment is alright.',
+            postId: this.post.id,
+            userId: this.user.id
+          })
+          .then((comment) => {
+            this.comment = comment;
+            done();
+          })
+        })
+      })
+    })
+
+    it('Should present a list of comments and posts a user has created', (done) => {
+      request.get(`${base}${this.user.id}`, (err, res, body) => {
+        expect(body).toContain('Snowball Fighting');
+        expect(body).toContain('This comment is alright.');
+        done();
+      })
+    })
+
+  })
 
 })
